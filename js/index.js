e96d5a9252cb7767f52c102bec71e8c78c47526e
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-24 18:44:35 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,271 @@
+// let num = +prompt('son kirit')
+// let max = num
+
+// for (let i = 1; i <= 4; i++){
+//     num = +prompt('son')
+//     if(num > max){
+//         num = max
+//     }
+// }
+// console.log(max);
+
+
+
+// let num = 1234
+// let res = 0
+// while(num <= 1){
+//     res =  res * 10 + res % 10
+//     num = Math.floor(num / 10)
+// }
+// console.log(res);
+
+
+
+// let fruits = ['olma', 'anor', 100, 'shaftoli', 50, true]
+// fruits[2] = 45
+// console.log(fruits);
+
+
+// fruits.push(458)
+// fruits.pop()
+// fruits.unshift('478')
+// fruits.shift()
+// console.log(fruits);
+
+
+// let arr = [3, 7, 10, 8]
+// let sum = 0
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+
+
+
+
+// 1-masala
+// let arr = [1, 2, 3, 5]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     arr1.push(arr[i] *= 2)
+// }
+// console.log(arr1);
+    
+
+// 2-masala
+// let arr = [1, 2, 3, 4]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     arr1.unshift(arr[i])
+// }
+// console.log(arr1);
+
+
+// 3-masaala
+// let arr = [1, -5, 8, -8, 5, -4]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 4-masala
+// let arr = [4, 5, 8, 9]
+// let sum = 0
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]
+// }
+// console.log(sum);
+
+
+// 5-masala
+// let arr = [1, 2, 3, 4]
+// arr.push(5)
+// console.log(arr);
+
+
+// 6-masala
+// let arr = [1, 2, 3, 4, 55]
+// arr.pop()
+// console.log(arr);
+
+
+// 7-masala
+// let arr = [5, 1, 2, 3, 4]
+// arr.shift()
+// console.log(arr);
+
+
+// 8-masala
+// let arr = [2, 3, 4, 5]
+// arr.unshift(1)
+// console.log(arr);
+
+
+// 9-masala
+// let arr =[10, 20, 13, 50, 8]
+// console.log(arr.indexOf(13));
+
+
+// 10-masala
+// let arr = [1, 2, 3, 4]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     arr1.push(arr[i])
+// }
+// console.log(arr1);
+
+
+// 11-masala
+// let arr = [12, 4, 24, 31, 16]
+// let max = arr[0]
+// for(let i = 0; i < arr.length; i++){
+//     if(max < arr[i]){
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+
+// 12-masala
+// let arr = [12, 4, 15, 24, 32]
+// let min = arr[0]
+// for(let i = 1; i < arr.length; i++){
+//     if(min > arr[i]){
+//         min = arr[i]
+//     }
+// }
+// console.log(min);
+
+
+// 13-masala
+// let arr = [12, 23, 10, 5, 77, 8]
+// let res = 0
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         res += 1
+//     }
+// }
+// console.log(res);
+
+
+// 14-masala
+// let arr = [12, 23, 10, 5, 77, 8]
+// let res = 0
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 == 1){
+//         res += 1
+//     }
+// }
+// console.log(res);
+
+
+// 15-masala
+// let arr = [12, 0, 5, 87, 0]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] !== 0){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 16-masala
+// let arr = [1, 2, 3, 4, 5]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     arr1.push(arr[i])
+//     arr1.push(arr[i])
+// }
+// console.log(arr1);
+
+
+// 17-masala
+// let arr = [1, 2, 3, 4, 5, 6]
+// let middle = Math.trunc(arr.length / 2)
+// let first = arr.slice(0, middle)
+// let last = arr.slice(middle)
+// console.log(first);
+// console.log(last);
+
+
+// 18-masala
+// let arr = [4, 5, 3, 2]
+// let res = 1
+// for(let i = 0; i < arr.length; i++){
+//     res *= arr[i]
+// }
+// console.log(res);
+
+
+// 19-masala
+// let arr = [4, 5, 2, 6]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     if(i % 2 == 1){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 20-masala
+// let arr = [4, 5, 2, 6]
+// let arr1 = []
+// for(let i = 0; i < arr.length; i++){
+//     if(i % 2 == 0){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 21-masala
+// let arr = ['hello', 12, 'anor', 54, 87]
+// let arr1 = []
+// for(let i = 0; i< arr.length; i++){
+//     if(typeof arr[i] === 'string'){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 22-masala
+// let arr = ['hello', 12, 'anor', 54, 87]
+// let arr1 = []
+// for(let i = 0; i< arr.length; i++){
+//     if(typeof arr[i] === 'number'){
+//         arr1.push(arr[i])
+//     }
+// }
+// console.log(arr1);
+
+
+// 23-masala
+let arr = [12, 54, 61, 23, 74]
+console.log(arr.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
